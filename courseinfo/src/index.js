0dcf70c4ea9707d88a2f1ae76a45d5a28bb0b3ee
strahinja <strahinjaa.va@gmail.com>
2020-07-16 23:08:25 +0200

    courseinfoFinal

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -10,43 +10,58 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.content} {props.exercises}
     </p>
   )
 }
 
 const Content = (props) => {
+  console.log(props)
+
   return (
-      //use fragment to keep the same structure as in the original component
+    /*  
+        use fragment to keep the same structure as in the original component;
+        for each part in the Parts array create a Part function and pas the
+        properies of the Part to the function as props, the map fuction returns an arrays of functions,
+        each function is called and the returned jsx renederd. I think;
+        key prop set to index of part to eliminate warning in Console.  
+    */      
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3}/>
+      {props.parts.map(part => <Part key={props.parts.indexOf(part)} content={part.name} exercises={part.exercises} />)}
     </>
   )
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} 
-              exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course['name']} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts.map(part => part.exercises)}  />
     </div>
   )
 }
